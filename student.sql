f9483783eb04b623a22c7779cf41618299ace6ea
A.NAGA NAVEEN CHOWDARY <113875164+naveenchowdary2324@users.noreply.github.com>
2022-10-12 15:38:36 +0530

    Create student.sql

---
+++ student.sql
@@ -0,0 +1,17 @@
+create database UNIVE;
+use UNIVE;
+create table student99(sno int primary key, sname varchar(30));
+insert into student99 values(1,'manideep');
+insert into student99 values(2,'naveen');
+insert into student99 values(3,'abhinay');
+insert into student99 values(4,'himaja');
+select *from student99;
+
+create table faculty99(empid int primary key, facultyname varchar(30));
+insert into faculty99 values(1,'gandhi');
+insert into faculty99 values(2,'anjana');
+insert into faculty99 values(3,'satpathy');
+insert into faculty99 values(4,'bhargavi');
+select *from faculty99;
+insert into faculty99 values(5,'biswas');
+insert into faculty99 values(6,'sudeep');
